efa2eb9cfdb962e3e3bc37c5d643a1343bf9cf74
Jean-Luc <jean-luc.a@web-projet.com>
2019-04-03 11:40:24 +0200

    First commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+"""
+    Algorithm #0.0.1
+    Declare var
+    Store some arithmetics into the var
+    Display result
+    Operands and Operators, functions
+"""
+resultat = 0 # Définition de la variable
+resultat = 2 + 2
+print(resultat)
