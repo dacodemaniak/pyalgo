6df416387215114e405fb156289c75973d701d64
Jean-Luc <jean-luc.a@web-projet.com>
2019-04-03 14:48:16 +0200

    Pretty display

--- rendu_monnaie.py
+++ rendu_monnaie.py
@@ -8,6 +8,13 @@ Use % to get rest of int div
 Use input() function to get a user entry
 """
 
+# Defines some vars
+nbBillet100 = 0
+nbBillet50 = 0
+nbBillet10 = 0
+nbPiece = 0
+leftCoins = 0
+
 # Accept an amount entry
 customerSomme = input("Somme en entrée : ")
 somme = int(customerSomme)
@@ -17,31 +24,21 @@ nbBillet100 = somme // 100
 left100 = somme % 100
 
 if left100 > 0:
-    # Keep It Simple and Stupid
-    print ("Billets de 100 : ", nbBillet100, " Reste : ", left100)
     # Next... got 50
     nbBillet50 = left100 // 50
     left50 = left100 % 50
 
 
     if left50 > 0: 
-        # Keep It Simple and Stupid
-        print ("Billets de 50 : ", nbBillet50, " Reste : ", left50)
         # Next... got 10
         nbBillet10 = left50 // 10
         left10 = left50 % 10
         
 
         if (left10 > 0):
-            # KISS
-            print ("Billets de 10 : ", nbBillet10, " Reste : ", left10)
             nbCoins = left10 // 2
             leftCoins = left10 % 2
 
-            if (nbCoins > 0):
-                print("et donc... ", nbCoins, " pièces de 2 euros. \nIl reste : ", leftCoins)
-            else: 
-                print("Il reste : ", leftCoins)
-else:
-    print("Vous m'avez donné ", nbBillet100, " merci...")
+# This is the end friends
+print("And the result is...\n", nbBillet100, " x 100\n", nbBillet50, " x 50\n", nbBillet10, " x 10\n", nbCoins, " x 2\n", leftCoins, " x units")
 
