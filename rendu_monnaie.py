f723dc657ecafaa2b39a950fca6df5e8befe3d1a
Jean-Luc <jean-luc.a@web-projet.com>
2019-04-03 14:39:45 +0200

    Poor KISS algorithme

--- rendu_monnaie.py
+++ rendu_monnaie.py
@@ -9,12 +9,39 @@ Use input() function to get a user entry
 """
 
 # Accept an amount entry
-customerSomme = input("Insérez votre billet : ")
+customerSomme = input("Somme en entrée : ")
 somme = int(customerSomme)
+
 # Got 100
-nbBillet100 = somme / 100
+nbBillet100 = somme // 100
 left100 = somme % 100
 
-# Just KISS (Keep It Simple and Stupid)
-print ("Il y a ", nbBillet100, " billets de 100 et il reste ", left100)
+if left100 > 0:
+    # Keep It Simple and Stupid
+    print ("Billets de 100 : ", nbBillet100, " Reste : ", left100)
+    # Next... got 50
+    nbBillet50 = left100 // 50
+    left50 = left100 % 50
+
+
+    if left50 > 0: 
+        # Keep It Simple and Stupid
+        print ("Billets de 50 : ", nbBillet50, " Reste : ", left50)
+        # Next... got 10
+        nbBillet10 = left50 // 10
+        left10 = left50 % 10
+        
+
+        if (left10 > 0):
+            # KISS
+            print ("Billets de 10 : ", nbBillet10, " Reste : ", left10)
+            nbCoins = left10 // 2
+            leftCoins = left10 % 2
+
+            if (nbCoins > 0):
+                print("et donc... ", nbCoins, " pièces de 2 euros. \nIl reste : ", leftCoins)
+            else: 
+                print("Il reste : ", leftCoins)
+else:
+    print("Vous m'avez donné ", nbBillet100, " merci...")
 
