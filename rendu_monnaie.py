5dc96f04954c83274792bda9c1082add36a9bc6b
Jean-Luc <jean-luc.a@web-projet.com>
2019-04-03 14:19:13 +0200

    KISS feature

---
+++ rendu_monnaie.py
@@ -0,0 +1,16 @@
+"""
+@name rendu_monnaie.py
+@author Aélion
+@version 0.0.1
+A poor coins counter
+Use // to get an integer division
+Use % to get rest of int div
+Use input() function to get a user entry
+"""
+
+# Accept an amount entry
+somme = input("Insérez votre billet : ")
+
+# Just KISS (Keep It Simple and Stupid)
+print("Vous m'avez donné " , somme , " euros")
+
